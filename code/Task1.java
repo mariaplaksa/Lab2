bbafc68ff84640809095059cbe508fb3cc0bb030
Мария Плакса <mariaplaksa6@gmail.com>
2025-11-13 09:49:49 +0300

    Update Task1.java

--- code/Task1.java
+++ code/Task1.java
@@ -1,1 +1,22 @@
-
+import java.io.PrintStream;
+import java.util.Scanner;
+public class Task1 {
+    public static Scanner in = new Scanner(System.in);
+    public static PrintStream out = System.out;
+    public static void main(String[] args)  {
+        int a0 = in.nextInt();
+        int n = in.nextInt();
+        int x = a0 * (-3);
+        out.print(x + " ");
+        for (int i = 1; i < n; i++) {
+            if (i % 2 != 0) {
+                x = x - 2;
+                out.print(x + " ");
+            }
+            else{
+                x = x * (-3);
+                out.print(x + " ");
+            }
+        }
+    }
+}
