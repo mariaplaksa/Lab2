e3aa1647db9086421da6a56fb8c1e0f443cc8756
Мария Плакса <mariaplaksa6@gmail.com>
2025-11-19 20:52:59 +0300

    Create Task4.java

---
+++ code/Task4.java
@@ -0,0 +1,32 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+public class Task4 {
+    public static Scanner in = new Scanner(System.in);
+    public static PrintStream out = System.out;
+
+    public static void main(String[] args) {
+        int n = in.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = in.nextInt();
+        }
+        int start = 0;
+        for (int i = 1; i < n; i++) {
+            if (a[i] != a[i - 1] + 1) {
+                if (start == i - 1) {
+                    out.print(a[start]);
+                    out.print(" ");
+                } else {
+                    out.print(a[start] + "-" + a[i - 1]);
+                    out.print(" ");
+                }
+                start = i;
+            }
+        }
+        if (start == n - 1) {
+            System.out.print(a[start]);
+        } else {
+            System.out.print(a[start] + "-" + a[n - 1]);
+        }
+    }
+}
